ab3a6c6aa322630005000548c585c4d7b57514cb
Jiaxin(Peter) Liu <peterliu8368@gmail.com>
2021-08-06 22:58:06 -0700

    eliminate if conditions

--- server.py
+++ server.py
@@ -5,6 +5,21 @@ from datetime import datetime
 app = Flask(__name__)
 app.secret_key = 'very secret key'
 
+buildings = ['Farm', 'Cave', 'House', 'Casino']
+gold_choice = ['(earns 10-20 golds)', '(earns 5-10 golds)', '(earns 2-5 golds)', '(earns/takes 0-50 golds)']
+
+gold_dictionary = {
+    'farm': random.randint(10,20),
+    'cave': random.randint(5,10),
+    'house': random.randint(2,5),
+    'casino': random.randint(-50,50),
+}
+
+def appendActivities(gold, choice):
+    session['gold'] += gold
+    session['activities'].append(f'<p>Earned {gold} golds from the {choice}! ({datetime.now()})</p>')
+
+#routes
 @app.route('/')
 def home_page():
     if 'gold' not in session:
@@ -12,31 +27,25 @@ def home_page():
 
     if 'activities' not in session:
         session['activities'] = []
+
+    if 'buildings' not in session:
+        session['buildings'] = ['Farm', 'Cave', 'House', 'Casino']
+
+    if 'gold_choice' not in session:
+        session['gold_choice'] = ['(earns 10-20 golds)', '(earns 5-10 golds)', '(earns 2-5 golds)', '(earns/takes 0-50 golds)']
+
     return render_template('index.html')
 
 @app.route('/process', methods = ['post'])
 def process():
     choice = request.form['choice']
     session['color'] = 'green'
-    if choice == 'farm':
-        gold = random.randint(10,20)
-        session['gold'] += gold
-        session['activities'].append(f'<p>Earned {gold} golds from the {choice}! ({datetime.now()})</p>')
-    elif choice == 'cave':
-        gold = random.randint(5,10)
-        session['gold'] += gold
-        session['activities'].append(f'<p>Earned {gold} golds from the {choice}! ({datetime.now()})</p>')
-    elif choice == 'house':
-        gold = random.randint(2,5)
-        session['gold'] += gold
-        session['activities'].append(f'<p>Earned {gold} golds from the {choice}! ({datetime.now()})</p>')
+    gold = gold_dictionary[choice]
+    if gold < 0:
+        session['gold'] += gold 
+        session['activities'].append(f'<p style="color: red">Lost {gold} golds from the {choice}! OOOOPS ({datetime.now()})</p>')
     else:
-        gold = random.randint(-50,50)
-        session['gold'] += gold
-        if gold < 0:
-            session['activities'].append(f'<p style="color: red">Lost {gold} golds from the {choice}! OOOOPS ({datetime.now()})</p>')
-        else:
-            session['activities'].append(f'<p>Earned {gold} golds from the {choice}! ({datetime.now()})</p>')
+        appendActivities(gold,choice)
 
     print(request.form['choice'])
 
